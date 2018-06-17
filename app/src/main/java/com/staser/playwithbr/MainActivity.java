a367e284e8c6ca9aef46e441e58417de4a4e6db9
StankovSergiy <sergstankov@gmail.com>
2018-06-17 22:09:05 +0300

    branch

--- app/src/main/java/com/staser/playwithbr/MainActivity.java
+++ app/src/main/java/com/staser/playwithbr/MainActivity.java
@@ -19,4 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
     }
+
+    @Override
+    public String toString() {
+        return "MainActivity{}";
+    }
 }
